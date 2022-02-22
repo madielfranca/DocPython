f3c26c955680c07b76d39d0bc822235b3733eedf
madielfranca <mdlsk819@gmail.com>
2022-02-22 11:52:23 -0300

    Diretorios

---
+++ Dicionario/Dicionario em Python.py
@@ -0,0 +1,20 @@
+aluno = dict()
+aluno['Nome'] = str(input('Digite o nome do(a) aluno(a): '))
+aluno['Média'] = float(input('Digite a média agora: '))
+
+if aluno['Média'] >= 7:
+    aluno['Situação'] = 'Aprovado'
+    print(f'O aluno {aluno["Nome"]} está aprovado!')
+elif aluno['Média'] >= 5 and aluno['Média'] < 7:
+    aluno['Situação'] = 'Na recuperação'
+    print(f'O aluno {aluno["Nome"]} está na recuperação!')
+else:
+    aluno['Situação'] = 'Reprovado'
+    print(f'O aluno {aluno["Nome"]} está reprovado!')
+
+print('-+' * 30)
+print('Dados: ')
+print(aluno)
+for k, v in aluno.items():
+    print(f'{k} é {v}')
+print('-+' * 30)
