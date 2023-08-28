de72ba2e56d42ed068a91737a7f58664d981dec5
madiel <madielfranca@gmail.com>
2023-08-28 11:23:33 -0300

    Fatorial

--- Funcoes/Funcoes.py
+++ Funcoes/Funcoes.py
@@ -6,3 +6,14 @@ def linhas(titulo):
 
 linhas('notas')
 linhas('alunos')
+
+
+def contador(i,f,p):
+    c = i
+    while c <= f:
+        print(c, end='\n')
+        print()
+        c +=p
+    print('fim!')
+
+contador(2,10,2)
