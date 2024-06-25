34802a7e732c98a1faf0966d30ba9730a0b420bb
madiel <madielfranca@gmail.com>
2024-06-25 20:44:06 -0300

    Tradução

--- Pandas/Hierarquia/src/LogStatus.py
+++ Pandas/Hierarquia/src/LogStatus.py
@@ -13,46 +13,32 @@ class LogStatus:
     @retry(3, Exception)
     def logar(self):
         valor_carga = self.valor_carga
-
-        # Get the current date
         current_date = datetime.now().strftime('%Y-%m-%d')
         log_date = datetime.now()
 
-        # Configure logging
+        # Configura o log
         logging.basicConfig(filename=f'app_{current_date}.log', filemode='a', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-        # Log the start of the script
-        logging.info('Script started.')
-
         try:
-            # Path to the existing Excel file
             file_path = f'HierarquiaStatus_{current_date}.xlsx'
-            
-            # Check if the file exists
-            if os.path.exists(file_path):
-                # Read the existing Excel file
-                existing_df = pd.read_excel(file_path)
-                logging.info(f'File {file_path} found and loaded.')
-            else:
-                # If the file does not exist, create an empty DataFrame with specified column names
-                existing_df = pd.DataFrame(columns=['Status'])
-                logging.info(f'File {file_path} not found. Created a new DataFrame.')
 
-            # Example list of values to add
+            if os.path.exists(file_path):
+                existing_df = pd.read_excel(file_path)
+            else:
+                # Se o arquivo não existir, cria um DataFrame vazio com nomes de colunas especificados
+                existing_df = pd.DataFrame(columns=['Status'])
+
             values_to_add_list = valor_carga
-            # Create a DataFrame with the new values
+            # Cria um DataFrame com os novos valores
 
             new_data = pd.DataFrame({'Status': values_to_add_list, 'Arquivo': self.file_name_carga, 'Data Execução': log_date})
 
-
-            # Append the new data to the existing DataFrame
+            # Anexa os novos dados ao DataFrame existente
             existing_df = pd.concat([existing_df, new_data], ignore_index=True)
-            logging.info(f'Added new values to the DataFrame.')
 
-            # Write the updated DataFrame back to the Excel file
+            # Grava o DataFrame atualizado de volta no arquivo Excel
             with pd.ExcelWriter(file_path, engine='openpyxl') as writer:
                 existing_df.to_excel(writer, index=False, sheet_name='Sheet1')
-                logging.info(f'Data written to {file_path}.')
 
         except Exception as e:
             logging.error(f'Error occurred: {e}')
